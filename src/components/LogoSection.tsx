78e497901eb707093f08f729db5a0684d160cdc7
Lyaguh NoteBook <artyom07206@gmail.com>
2025-07-10 15:33:04 +0300

    select dices

--- src/components/LogoSection.tsx
+++ src/components/LogoSection.tsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+import { useRollDiceStore } from "../stores/RollDiceStore";
+
 export default function LogoSection() {
+  const [cnt, setCnt] = useState<number>(0);
+  const { setSelectedDice } = useRollDiceStore();
+
+  const handleClick = () => {
+    setCnt(cnt + 1);
+    if (cnt >= 5) {
+      setSelectedDice(100);
+    }
+  };
+
   return (
     <>
       <div className="flex w-full justify-center pt-main">
-        <img src="src\assets\d20.svg" className="size-24" />
+        <img
+          src="src\assets\d20.svg"
+          className="size-24 active:scale-95 "
+          onClick={handleClick}
+        />
       </div>
       <div className="flex justify-center py-main">
         <h1 className="text-white text-2xl font-bold">Dice Roller</h1>
--- src/components/RollDiceBoxSection.tsx
+++ src/components/RollDiceBoxSection.tsx
@@ -1,5 +1,30 @@
+import { useRollDiceStore } from "../stores/RollDiceStore";
+
 export default function RollDiceBoxSection() {
+  const { selectedDice, resultRoll } = useRollDiceStore();
   return (
-    <div className="h-[200px] w-full rounded-main bg-white/5 backdrop-blur-sm border border-white/20 "></div>
+    <div className="h-[200px] flex justify-center items-center w-full rounded-main bg-white/5 backdrop-blur-sm border border-white/20 ">
+      {selectedDice === 20 && (
+        <div className="hexagon">{resultRoll ?? selectedDice}</div>
+      )}
+      {selectedDice === 12 && (
+        <div className="octagon">{resultRoll ?? selectedDice}</div>
+      )}
+      {selectedDice === 10 && (
+        <div className="rhombus">{resultRoll ?? selectedDice}</div>
+      )}
+      {selectedDice === 8 && (
+        <div className="triangle">{resultRoll ?? selectedDice}</div>
+      )}
+      {selectedDice === 6 && (
+        <div className="square">{resultRoll ?? selectedDice}</div>
+      )}
+      {selectedDice === 4 && (
+        <div className="triangle">{resultRoll ?? selectedDice}</div>
+      )}
+      {selectedDice === 100 && (
+        <div className="d100">{resultRoll ?? selectedDice}</div>
+      )}
+    </div>
   );
 }
